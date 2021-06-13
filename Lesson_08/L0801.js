7dd526819b51b18ae8d4af5d1314f03186144200
Ankit Goyal <ankitgoyal@outlook.com.au>
2021-06-14 09:55:32 +1000

    ANKs

--- Lesson_08/L0801.js
+++ Lesson_08/L0801.js
@@ -8,3 +8,25 @@ gs.info(typeof iStr);
 
 var n = parseInt(iStr);
 gs.info(typeof n + ' n=' + n);
+
+
+/*
+Create 3 string variables 
+Print the lenght of each string
+concatenate them together and using new line character, save it to a new variable 
+then print the lenght of the new variable
+*/
+
+var myName = "Anks"
+var myPhone = "0430361020"
+var myEmail = "anks.aqua@yahoo.com"
+
+gs.info('My Name lenght is ' + myName.length);
+gs.info('My Phone lenght is ' + myPhone.length);
+gs.info('My Email lenght is ' + myEmail.length);
+
+var myDetails = myName + '\n' + myPhone + '\n' + myEmail;
+
+gs.info(myDetails);
+
+gs.info(myDetails.length);
--- Lesson_14/L14S03.js
+++ Lesson_14/L14S03.js
@@ -10,3 +10,28 @@ var string3 = 'Hello, world!';
 gs.info('string1=' + ((string1) ? 'true' : 'false'));
 gs.info('string2=' + ((string2) ? 'true' : 'false'));
 gs.info('string3=' + ((string3) ? 'true' : 'false'));
+
+// Lab 4:
+// Create a script that takes a string variable "Hello world"
+// and translates it to at least 3 different languages
+// based on a 'language' variable.
+// Make the default language English if a match isn't found.
+
+
+var sayHello = "Hello world"; // Hola Mundo, こんにちは世界, ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ ਦੁਨਿਆ
+var message = '';
+
+switch (sayHello) {
+    case "ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ ਦੁਨਿਆ":
+        message = 'Punjabi';
+        break;
+    case 'Hola Mundo':
+        message = 'Spanish';
+        break;
+    case 'こんにちは世界':
+        message = 'Japanese'
+        break;
+    default:
+        message = "I don't know"
+}
+gs.info('Name is the language is ' + message);
